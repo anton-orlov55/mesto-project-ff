33096dbd02eda0b97e10cccdb4f70ff42386cb9d
Orlov Anton <obucenia232323@gmail.com>
2025-05-16 18:50:27 +0500

    Исправленная версия 3 итерация 16.05.25

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,41 +1,36 @@
+let openedPopup = null; 
 
-const handleEscClose = (evt, closeModal) => {
-  if (evt.key === 'Escape') closeModal();
+
+const handleEscClose = (evt) => {
+  if (evt.key === 'Escape' && openedPopup) {
+    closeModal(openedPopup);
+  }
 };
-
 
 export const openModal = (modalElement) => {
+ 
+  if (openedPopup) {
+    closeModal(openedPopup);
+  }
   modalElement.classList.add('popup_is-opened');
-  
-
-  const escHandler = (evt) => handleEscClose(evt, () => closeModal(modalElement));
-  document.addEventListener('keydown', escHandler);
-  
-
-  return () => document.removeEventListener('keydown', escHandler);
+  openedPopup = modalElement;
+  document.addEventListener('keydown', handleEscClose);
 };
-
 
 export const closeModal = (modalElement) => {
   modalElement.classList.remove('popup_is-opened');
+  if (openedPopup === modalElement) {
+    openedPopup = null;
+    document.removeEventListener('keydown', handleEscClose);
+  }
 };
 
-
 export const setupModalCloseListeners = (modalElements) => {
-  const cleanups = [];
-  
   modalElements.forEach(modalElement => {
-    const clickHandler = (evt) => {
+    modalElement.addEventListener('mousedown', (evt) => {
       if (evt.target === modalElement || evt.target.closest('.popup__close')) {
         closeModal(modalElement);
       }
-    };
-    
-    modalElement.addEventListener('mousedown', clickHandler);
-    cleanups.push(() => {
-      modalElement.removeEventListener('mousedown', clickHandler);
     });
   });
-  
-  return () => cleanups.forEach(cleanup => cleanup());
 };
